f48bb7d1eccd52597f654d72e3354e3e507c9bb2
whythat <whythat@protonmail.com>
2017-05-06 16:51:19 +0300

    Add tests for seek operations

--- secfile_test.go
+++ secfile_test.go
@@ -1,6 +1,7 @@
 package secfile
 
 import (
+	"io"
 	"os"
 	"testing"
 
@@ -17,7 +18,7 @@ func TestWriteRead(t *testing.T) {
 	assert.Nil(t, write([]byte(data), os.O_CREATE|os.O_WRONLY))
 	res, err := read(len(data), os.O_RDONLY)
 	assert.Nil(t, err)
-	assert.Equal(t, string(res), data)
+	assert.Equal(t, data, string(res))
 	assert.Nil(t, cleanUp())
 }
 
@@ -28,15 +29,63 @@ func TestWriteAppendRead(t *testing.T) {
 	assert.Nil(t, write([]byte(data2), os.O_APPEND|os.O_WRONLY))
 	res, err := read(len(data1)+len(data2), os.O_RDONLY)
 	assert.Nil(t, err)
-	assert.Equal(t, string(res), data1+data2)
+	assert.Equal(t, data1+data2, string(res))
 	assert.Nil(t, cleanUp())
 }
 
-// TODO: see TODO note in secfile.go
-func TestRDWR(t *testing.T) {
-	assert.NotNil(t, write([]byte("anything"), os.O_CREATE|os.O_RDWR))
-	_, err := os.Stat(filename)
-	assert.True(t, os.IsNotExist(err))
+func TestSeekStartRead(t *testing.T) {
+	data := "This is some data."
+	assert.Nil(t, write([]byte(data), os.O_CREATE|os.O_WRONLY))
+	res, err := seekRead(5, io.SeekStart, 2, os.O_RDONLY)
+	assert.Nil(t, err)
+	assert.Equal(t, "is", string(res))
+	assert.Nil(t, cleanUp())
+}
+
+func TestSeekEndRead(t *testing.T) {
+	data := "This is some data."
+	assert.Nil(t, write([]byte(data), os.O_CREATE|os.O_WRONLY))
+	res, err := seekRead(-5, io.SeekEnd, 4, os.O_RDONLY)
+	assert.Nil(t, err)
+	assert.Equal(t, "data", string(res))
+	assert.Nil(t, cleanUp())
+}
+
+func TestSeekCurrentRead(t *testing.T) {
+	data := "This is some data."
+	f, err := Open(filename, []byte(key), os.O_CREATE|os.O_RDWR, 0666)
+	assert.Nil(t, err)
+	defer f.Close()
+	_, err = f.Write([]byte(data))
+	assert.Nil(t, err)
+	_, err = f.Seek(-5, io.SeekCurrent)
+	assert.Nil(t, err)
+	res := make([]byte, 4)
+	_, err = f.Read(res)
+	assert.Nil(t, err)
+	assert.Equal(t, "data", string(res))
+	assert.Nil(t, cleanUp())
+}
+
+func TestSeekCurrentWrite(t *testing.T) {
+	data := "This is data."
+	piece := "appended data."
+	f, err := Open(filename, []byte(key), os.O_CREATE|os.O_RDWR, 0666)
+	assert.Nil(t, err)
+	defer f.Close()
+	_, err = f.Write([]byte(data))
+	assert.Nil(t, err)
+	_, err = f.Seek(-5, io.SeekCurrent)
+	assert.Nil(t, err)
+	_, err = f.Write([]byte(piece))
+	assert.Nil(t, err)
+	_, err = f.Seek(0, io.SeekStart)
+	assert.Nil(t, err)
+	res := make([]byte, len(data)-5+len(piece))
+	_, err = f.Read(res)
+	assert.Nil(t, err)
+	assert.Equal(t, "This is appended data.", string(res))
+	assert.Nil(t, cleanUp())
 }
 
 func write(data []byte, flags int) error {
@@ -49,6 +98,23 @@ func write(data []byte, flags int) error {
 		return err
 	}
 	return nil
+}
+
+func seekRead(offset int64, whence, len, flags int) ([]byte, error) {
+	f, err := Open(filename, []byte(key), flags, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	_, err = f.Seek(offset, whence)
+	if err != nil {
+		return nil, err
+	}
+	data := make([]byte, len)
+	if _, err := f.Read(data); err != nil {
+		return nil, err
+	}
+	return data, nil
 }
 
 func read(len, flags int) ([]byte, error) {
